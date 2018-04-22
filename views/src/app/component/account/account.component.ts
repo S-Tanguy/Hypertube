40649d136fcf411976e8215053ae3b9fa6c49f0b
Stanya PALMARO <spalmaro@e2r10p11.42.fr>
2018-04-22 19:32:06 +0200

    formulaire pour changer les informations du compte

--- views/src/app/component/account/account.component.ts
+++ views/src/app/component/account/account.component.ts
@@ -7,13 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountComponent implements OnInit {
   user = { picture: '', login: '', given_name: '', family_name: '', email: '', password: '', language: 'en' }
-  
+  noMatch = true;
+  password = '';
+  repeatpwd = '';
+  upload = false;
+
   constructor() { }
 
   ngOnInit() {
   }
 
   changeLang() {
+
+  }
+
+  passwordMatch() {
+    if (this.password !== this.repeatpwd) {
+      this.noMatch = true;
+    } else {
+      this.noMatch = false;
+    }
+  }
+
+  updateProfile() {
+
+  }
+
+  changePassword() {
+
+  }
+
+  uploadPicture(event) {
 
   }
 
